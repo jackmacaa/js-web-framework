8755792b666a7c6b684f447c019cb2cdbcc89235
jack.mcloughlin <jackmaca@msn.com>
2023-01-27 13:54:19 +1030

    added on even funcion

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,5 @@ import { User } from './models/User';
 
 const user = new User({ name: 'jack', age: 32 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
-
-user.set({ name: 'jackmac' });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {});
+console.log(user);
