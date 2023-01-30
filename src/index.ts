fbbb96485da0993a63c51370692b24ae983150e7
jack.mcloughlin <jackmaca@msn.com>
2023-01-30 16:28:48 +1030

    added json server and axios

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,10 @@ import { User } from './models/User';
 const user = new User({ name: 'jack', age: 32 });
 
 user.on('change', () => {});
-console.log(user);
+user.on('save', () => {
+  console.log('Save was triggered');
+});
+
+user.trigger('save')
+
+//console.log(user)
